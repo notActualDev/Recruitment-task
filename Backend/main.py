4d953f575f4e4543a56d067cc789b64ade2bb3ac
notActualDev <notactualdev@gmail.com>
2026-03-26 17:04:32 +0100

    backend CORS open

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # pozwól na wszystkie domeny
+    allow_credentials=True,
+    allow_methods=["*"],   # wszystkie metody (GET, POST itd.)
+    allow_headers=["*"],   # wszystkie nagłówki
+)
 
 @app.get("/")
 def root():
