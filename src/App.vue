7d589b838ae6268bcf785e0e92a370c111b05222
notActualDev <notactualdev@gmail.com>
2026-03-26 11:42:19 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(69)
+
+function addOne()
+{
+  counter.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="addOne">+1</button>
+  </div>
+</template>
