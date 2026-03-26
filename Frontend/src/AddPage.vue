c7d7ae1c770a4d579c0b9eb2ba54a0d9f446e3b2
notActualDev <notactualdev@gmail.com>
2026-03-26 16:47:53 +0100

    Connection: Frontend to Backend

--- Frontend/src/AddPage.vue
+++ Frontend/src/AddPage.vue
@@ -2,9 +2,27 @@
 import { ref } from 'vue'
 
 const counter = ref(420)
+const text = ref('')
+
+// adres backendu z Vercel Environment Variables
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 function addOne() {
   counter.value++
+}
+
+async function getText() {
+  try {
+    const response = await fetch(`${backendUrl}/`)
+    const data = await response.text()
+    text.value = data
+  } catch (error) {
+    text.value = 'Error while fetching data'
+  }
+}
+
+function clearText() {
+  text.value = ''
 }
 </script>
 
@@ -18,5 +36,17 @@ function addOne() {
     <router-link to="/minus">
       Go to minus page
     </router-link>
+
+    <br><br>
+
+    <!-- pole wyświetlania tekstu -->
+    <div style="border:1px solid black; padding:10px; min-height:50px;">
+      {{ text }}
+    </div>
+
+    <br>
+
+    <button @click="getText">Get</button>
+    <button @click="clearText">Clear</button>
   </div>
 </template>
